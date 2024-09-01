dac416a4cdade7f85e607a84f121bc972bf0fba3
nvnkmrr <nvnkmr.r@gmail.com>
2024-09-01 14:47:00 +0530

    code optimization

--- src/main/java/com/aupnmt/service/impl/CommonServiceImpl.java
+++ src/main/java/com/aupnmt/service/impl/CommonServiceImpl.java
@@ -1,5 +1,10 @@
 package com.aupnmt.service.impl;
 
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,35 +26,94 @@ public class CommonServiceImpl implements CommonService {
 	@Autowired
 	AdminService adminService;
 
+	@Autowired
+	AzureStorageServiceImpl azureStorageServiceImpl;
+
 	@Override
 	public AccessToken userIdentification(String phoneNumber) throws Exception {
 		AccessToken accessToken = new AccessToken();
-		String employeeIdentification = employeeService.findEmployee(phoneNumber);
-		String ownerIdentification = ownerService.findOwner(phoneNumber);
-		String adminIdentification = adminService.findAdmin(phoneNumber);
+		
+		String employeeIdentification = "";
+		String ownerIdentification = "";
+		String adminIdentification = "";
+		String employeeName = "";
+		String ownerName = "";
+		String adminName = "";
+		XSSFWorkbook workbook = azureStorageServiceImpl.readDatabase();
+		XSSFSheet xSSFSheet = workbook.getSheet("Employee");
+		Iterator<Row> rowIterator = xSSFSheet.iterator();
+		boolean skipHeader = true;
+		while (rowIterator.hasNext()) {
+			// skipping header
+			if (skipHeader) {
+				rowIterator.next();
+				skipHeader = false;
+			}
+			Row row = (Row) rowIterator.next();
+			if (row.getCell(2).toString().equalsIgnoreCase(phoneNumber)) {
+				employeeName = row.getCell(1).toString();
+				employeeIdentification = "Success" + row.getCell(13).toString();
+			}
+		}
+		employeeIdentification = employeeIdentification.isEmpty() ? "Failure" : employeeIdentification;
+
+		xSSFSheet = workbook.getSheet("Owner");
+		rowIterator = xSSFSheet.iterator();
+		skipHeader = true;
+		while (rowIterator.hasNext()) {
+			// skipping header
+			if (skipHeader) {
+				rowIterator.next();
+				skipHeader = false;
+			}
+			Row row = (Row) rowIterator.next();
+			if (row.getCell(2).toString().equalsIgnoreCase(phoneNumber)) {
+				ownerName = row.getCell(1).toString();
+				ownerIdentification = "Success" + row.getCell(12).toString();
+			}
+		}
+		ownerIdentification = ownerIdentification.isEmpty() ? "Failure" : ownerIdentification;
+
+		xSSFSheet = workbook.getSheet("Admin");
+		rowIterator = xSSFSheet.iterator();
+		skipHeader = true;
+		while (rowIterator.hasNext()) {
+			// skipping header
+			if (skipHeader) {
+				rowIterator.next();
+				skipHeader = false;
+			}
+			Row row = (Row) rowIterator.next();
+			adminName = row.getCell(1).toString();
+			if (row.getCell(2).toString().equalsIgnoreCase(phoneNumber)) {
+				adminIdentification = "Success";
+			}
+		}
+		adminIdentification = adminIdentification.isEmpty() ? "Failure" : adminIdentification;
+		
 		if (employeeIdentification.equalsIgnoreCase("SuccessY")) {
 			accessToken.setRole("EMPLOYEE");
 			accessToken.setPremiumUser(true);
-			accessToken.setName(employeeService.employee(phoneNumber).getName());
+			accessToken.setName(employeeName);
 		} else if (employeeIdentification.equalsIgnoreCase("SuccessN")) {
 			accessToken.setRole("EMPLOYEE");
 			accessToken.setPremiumUser(false);
-			accessToken.setName(employeeService.employee(phoneNumber).getName());
+			accessToken.setName(employeeName);
 		}
 
 		if (ownerIdentification.equalsIgnoreCase("SuccessY")) {
 			accessToken.setRole("OWNER");
 			accessToken.setPremiumUser(true);
-			accessToken.setName(ownerService.owner(phoneNumber).getName());
+			accessToken.setName(ownerName);
 		} else if (ownerIdentification.equalsIgnoreCase("SuccessN")) {
 			accessToken.setRole("OWNER");
 			accessToken.setPremiumUser(false);
-			accessToken.setName(ownerService.owner(phoneNumber).getName());
+			accessToken.setName(ownerName);
 		}
 
 		if (adminIdentification.equalsIgnoreCase("Success")) {
 			accessToken.setRole("ADMIN");
-			accessToken.setName(adminService.admin(phoneNumber).getName());
+			accessToken.setName(adminName);
 		}
 
 		return accessToken;
