ce8866c6f37ae9af65098f06528bc33aa56a1fca
nvnkmrr <nvnkmr.r@gmail.com>
2024-10-01 11:21:29 +0530

    converted put to post

--- src/main/java/com/aupnmt/api/CommonController.java
+++ src/main/java/com/aupnmt/api/CommonController.java
@@ -12,7 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -79,7 +79,7 @@ public class CommonController {
 		return response;
 	}
 
-	@PutMapping("/user/delete")
+	@PostMapping("/user/delete")
 	public Response delete(@RequestParam String phoneNumber) {
 		Response r = new Response();
 		try {
